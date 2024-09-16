d59bc67ea143e0fe4de3b16e735b11542d2556a6
keith-woolley <kwoolley2@mail.pima.edu>
2024-09-15 23:05:43 -0700

    Add short story and remove "Hello World".

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Keith Woolley
+# The program will write a short story.
+# Printing "Hello World" was removed and replaced with the short story.
+print('There was an earthworm.')
+print('It wanted to live above the ground.')
+print('But it was too slow to live there.')
+print('Until and apple fell from a tree and it crawled inside.')
+print('Then it was able to roll around.')
